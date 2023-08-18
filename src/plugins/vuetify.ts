e232c0be3b9623d0f5fc1654d99ba6f273c8a4f4
Adam Zdebel <adam.zdebel1997@gmail.com>
2023-08-18 20:27:15 +0200

    change file structure

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -16,9 +16,9 @@ import { createVueI18nAdapter } from 'vuetify/locale/adapters/vue-i18n'
 
 import i18n from './i18n'
 
-import defaults from '@/config/defaults'
-import display from '@/config/display'
-import theme from '@/config/theme'
+import defaults from '@/config/vuetify/defaults'
+import display from '@/config/vuetify/display'
+import theme from '@/config/vuetify/theme'
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
