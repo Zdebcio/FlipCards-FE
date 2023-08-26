a411ee44514ef11e0a00588e34f7f5d8007da932
Adam Zdebel <adam.zdebel1997@gmail.com>
2023-08-26 20:22:17 +0200

    create auto-generating drawer

--- src/layouts/default/AppDrawer.vue
+++ src/layouts/default/AppDrawer.vue
@@ -1,15 +1,26 @@
 <script lang="ts" setup>
+import { RouteRecordRaw } from 'vue-router'
+
 import { useDisplay } from 'vuetify'
+
+import router from '../../router'
 
 defineProps<{
   open: boolean
 }>()
 
-defineEmits<{
+const emit = defineEmits<{
   (e: 'openDrawer', value?: boolean): void
 }>()
 
 const { mobile } = useDisplay()
+
+const handleOpenStatus = (status: boolean) => emit('openDrawer', status)
+
+const drawerRoutes = router.options.routes.filter(route => route.meta?.drawer)
+
+const getChildrenRoutes = (route: RouteRecordRaw) =>
+  route.children?.filter(route => route.meta?.drawer)
 </script>
 
 <template>
@@ -21,15 +32,21 @@ const { mobile } = useDisplay()
     :border="0"
     :elevation="0"
   >
-    <v-list>
-      <v-list-item></v-list-item>
+    <v-list v-for="route in drawerRoutes" :key="route.name">
+      <v-list-item
+        v-for="childRoute in getChildrenRoutes(route)"
+        :key="childRoute.name"
+        :to="`${route.path}${childRoute.path}`"
+        @click="handleOpenStatus(false)"
+        >{{ childRoute.name }}</v-list-item
+      >
     </v-list>
   </v-navigation-drawer>
   <v-overlay
     scrim="overlay"
     v-if="mobile"
     :model-value="open"
-    @click="$emit('openDrawer', false)"
+    @click="handleOpenStatus(false)"
     persistent
     :z-index="1003"
   ></v-overlay>
