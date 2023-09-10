4983babe10e2a8949d1e674f26ee8c6dd3bd839a
Adam Zdebel <adam.zdebel1997@gmail.com>
2023-09-11 01:05:15 +0200

    add-responsiveness-for-auth-pages

--- src/components/TextField.vue
+++ src/components/TextField.vue
@@ -1,11 +1,28 @@
 <script lang="ts" setup></script>
 
 <template>
-  <v-text-field class="textfield"></v-text-field>
+  <v-text-field class="textfield text-body-1"></v-text-field>
 </template>
 
 <style lang="scss" scoped>
 .textfield:deep() {
+  & .v-field {
+    font-size: inherit;
+
+    & .v-field__input {
+      padding: 1rem;
+      min-height: auto;
+    }
+  }
+
+  & .v-input__details {
+    padding: 0.1rem 1rem 0;
+    margin-bottom: 0.2rem;
+  }
+
+  & .v-messages {
+    font-size: inherit;
+  }
   & .v-field--variant-outlined .v-field__outline {
     border-radius: 15px;
     --v-field-border-width: 2px;
