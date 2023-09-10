3d630dff0f3798e12470b039313bee29a50f6bea
Adam Zdebel <adam.zdebel1997@gmail.com>
2023-09-11 00:49:34 +0200

    Change Dashboard navigation structure

---
+++ src/components/TilesNav.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup>
+import { useDisplay } from 'vuetify'
+
+import Tile from './Tile.vue'
+
+import type { Tile as TileType } from '../interfaces'
+
+const { thresholds } = useDisplay()
+
+defineProps<{
+  tiles: TileType[]
+}>()
+</script>
+
+<template>
+  <nav class="w-100 d-flex flex-column align-center">
+    <v-list class="w-100" :max-width="thresholds.xl">
+      <v-container fluid>
+        <v-row>
+          <v-col
+            class="pa-1"
+            v-for="tile in tiles"
+            :key="tile.title"
+            :cols="tile.cols"
+            :sm="tile.sm"
+            :xl="tile.xl"
+          >
+            <Tile :title="tile.title" :bg-color="tile.bgColor" :icon="tile.icon" :to="tile.to">
+              <template v-if="tile.content" v-slot:content>
+                <component :is="tile.content"></component>
+              </template>
+            </Tile>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-list>
+  </nav>
+</template>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,7 +1,59 @@
 <script lang="ts" setup>
-import DashboardNav from '@/components/DashboardNav.vue'
+import type { Tile } from '../interfaces'
+
+import PlaylistPlayButton from '@/components/PlaylistPlayButton.vue'
+import TilesNav from '@/components/TilesNav.vue'
+import ChartBarIcon from '@/components/icons/ChartBarIcon.vue'
+import CreateListIcon from '@/components/icons/CreateListIcon.vue'
+import ListIcon from '@/components/icons/ListIcon.vue'
+import ReplayIcon from '@/components/icons/ReplayIcon.vue'
+
+const tiles: Tile[] = [
+  {
+    title: 'Last opened',
+    icon: ReplayIcon,
+    bgColor: 'blue',
+    // TODO: change when page will create
+    to: '/',
+    cols: 12,
+    sm: 12,
+    xl: 8,
+    content: PlaylistPlayButton
+  },
+  {
+    title: 'All created lists',
+    icon: ListIcon,
+    bgColor: 'pink',
+    // TODO: change when page will create
+    to: '/',
+    cols: 12,
+    sm: 6,
+    xl: 4
+  },
+  {
+    title: 'Statistics',
+    icon: ChartBarIcon,
+    bgColor: 'orange',
+    // TODO: change when page will create
+    to: '/',
+    cols: 12,
+    sm: 6,
+    xl: 4
+  },
+  {
+    title: 'Create new list',
+    icon: CreateListIcon,
+    bgColor: 'green',
+    // TODO: change when page will create
+    to: '/',
+    fullWidth: true,
+    cols: 12,
+    sm: 12,
+    xl: 8
+  }
+]
 </script>
 
 <template>
-  <DashboardNav />
+  <TilesNav :tiles="tiles" />
 </template>
