f7271b3dcfca325bd91cfd81d1ec655852d7c01b
Adam Zdebel <adam.zdebel1997@gmail.com>
2024-01-22 23:29:29 +0100

    feat: Add links to drawer

--- src/router/index.ts
+++ src/router/index.ts
@@ -18,13 +18,13 @@ const routes = [
       {
         path: 'list/new',
         name: 'CreateList',
-        meta: { exact: true },
+        meta: { drawer: true, exact: true },
         component: () => import('@/views/CreateList.vue')
       },
       {
         path: 'lists',
         name: 'UserLists',
-        meta: { exact: true },
+        meta: { drawer: true, exact: true },
         component: () => import('@/views/UserLists.vue')
       }
     ]
