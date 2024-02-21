7a7ea6ebf1d7af3c9a0b8d4c9c20653503e8a68d
Adam Zdebel <adam.zdebel1997@gmail.com>
2024-02-21 17:30:16 +0100

    feat: Move list form to separate component

--- src/views/CreateList.vue
+++ src/views/CreateList.vue
@@ -1,48 +1,9 @@
 <script lang="ts" setup>
 import { useI18n } from 'vue-i18n'
 
-import { type PublicPathState, useForm } from 'vee-validate'
+import CreateListForm from '@/components/CreateListForm.vue'
 
-import TextField from '@/components/TextField.vue'
-import paths from '@/constants/paths'
-import { useCreateList } from '@/hooks'
-import router from '@/router'
-import { listNameSchema } from '@/schemas'
-
-const { defineComponentBinds, handleSubmit } = useForm({
-  validationSchema: listNameSchema
-})
-
-const { mutateAsync, error, isLoading, isError, reset } = useCreateList()
 const { t } = useI18n()
-
-const displayApiError = () => {
-  if (!isError.value) return ''
-  if (error.value?.response?.status === 422) {
-    return `${t('validation.invalidListName')}. ${t('validation.tooLong', { max: 20 })}`
-  }
-
-  return t('validation.otherForm')
-}
-
-const vuetifyConfig = (state: PublicPathState) => ({
-  props: {
-    'error-message': state.errors[0] || displayApiError()
-  },
-  validateOnValueUpdate: true
-})
-
-const listName = defineComponentBinds('name', vuetifyConfig)
-
-const onSubmit = handleSubmit(async (values, actions) => {
-  try {
-    await mutateAsync(values)
-    actions.resetForm()
-    router.push(paths.HOME)
-  } catch (error) {
-    actions.resetField('name')
-  }
-})
 </script>
 
 <template>
@@ -50,36 +11,12 @@ const onSubmit = handleSubmit(async (values, actions) => {
     <header class="d-flex w-100 justify-space-between align-center py-2">
       <h1>{{ t('createList.title') }}</h1>
     </header>
-    <form @submit.prevent="onSubmit" class="d-flex flex-column align-center py-2">
-      <div class="w-100 d-flex flex-column align-center">
-        <TextField
-          :error-messages="listName['error-message']"
-          :label="t('createList.nameLabel')"
-          :placeholder="t('createList.namePlaceholder')"
-          @change="reset()"
-          class="w-100 list-name-input"
-          id="list-name"
-          name="name"
-          v-bind="listName"
-        />
-      </div>
-      <v-btn
-        :disabled="!!listName['error-message']"
-        :loading="isLoading"
-        type="submit"
-        class="mt-2"
-        >{{ t('createList.button') }}</v-btn
-      >
-    </form>
+    <CreateListForm />
   </div>
 </template>
 
 <style lang="scss" scoped>
 header {
   border-bottom: 2px solid rgb(var(--v-theme-background-darken-1));
-}
-
-.list-name-input:deep() {
-  max-width: 500px;
 }
 </style>
