48776343cb39137a9060606df702942e68683056
Adam Zdebel <adam.zdebel1997@gmail.com>
2024-02-22 21:28:44 +0100

    fix: fix navbar template

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -26,9 +26,14 @@ const { mobile } = useDisplay()
     scroll-threshold="220"
   >
     <div class="w-100 d-flex justify-space-between align-center">
-      <MenuButton :visible="!!mobile" :open="open" @click="$emit('openDrawer')" />
-      <LogoIcon height="50px" />
-      <div class="d-flex align-center">
+      <MenuButton
+        :visible="!!mobile"
+        :open="open"
+        @click="$emit('openDrawer')"
+        class="w-50 justify-start d-inline-flex align-center"
+      />
+      <LogoIcon height="50px" class="d-inline-flex align-center flex-shrink-0" />
+      <div class="w-50 justify-end d-inline-flex align-center">
         <ThemeSwitcher class="me-5" />
         <v-btn-icon color="primary" class="mx-2">
           <!-- TODO: integrate when API provided -->
