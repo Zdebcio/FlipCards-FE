10fc1b2a5c1488b17921dede538415b1e4aed539
Adam Zdebel <adam.zdebel1997@gmail.com>
2024-02-26 22:29:41 +0100

    fix: remove unnecessary div

--- src/components/LanguageSelector.vue
+++ src/components/LanguageSelector.vue
@@ -4,27 +4,25 @@ import { LOCAL_STORAGE } from '@/constants'
 </script>
 
 <template>
-  <div class="language-wrapper">
-    <v-select
-      v-model="$i18n.locale"
-      :items="$i18n.availableLocales.map(value => ({ title: value.toUpperCase(), value }))"
-      bg-color="transparent"
-      color="transparent"
-      flat
-      variant="solo"
-      single-line
-      hide-no-data
-      hide-selected
-      hide-details
-      @update:model-value="(val: string) => setItem(LOCAL_STORAGE.LANG, val)"
-    >
-      <template v-slot:append-inner></template>
-    </v-select>
-  </div>
+  <v-select
+    v-model="$i18n.locale"
+    :items="$i18n.availableLocales.map(value => ({ title: value.toUpperCase(), value }))"
+    bg-color="transparent"
+    color="transparent"
+    flat
+    variant="solo"
+    single-line
+    hide-no-data
+    hide-selected
+    hide-details
+    @update:model-value="(val: string) => setItem(LOCAL_STORAGE.LANG, val)"
+  >
+    <template v-slot:append-inner></template>
+  </v-select>
 </template>
 
 <style lang="scss" scoped>
-.language-wrapper {
-  width: 5.3rem;
+.v-select {
+  max-width: 5.3rem;
 }
 </style>
