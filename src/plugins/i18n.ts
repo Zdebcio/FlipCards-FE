20371491a0226237860d38657fdd832e3769dc1c
Adam Zdebel <adam.zdebel1997@gmail.com>
2024-02-26 22:31:32 +0100

    FC-45 Add language switcher (#32)
    * feat: Create language component
    
    * feat: Save selected language in local storage
    
    * fix: remove unnecessary div
    
    * fix: sort props

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -2,9 +2,12 @@ import { createI18n, type I18nOptions } from 'vue-i18n'
 
 import messages from '@intlify/unplugin-vue-i18n/messages'
 
+import { getItem } from '@/config/localStorage'
+import { LOCAL_STORAGE } from '@/constants'
+
 export default createI18n({
   legacy: false,
-  locale: 'en',
+  locale: getItem(LOCAL_STORAGE.LANG) ?? navigator.language.split('-')[0],
   fallbackLocale: 'en',
   messages: messages as I18nOptions['messages']
 })
